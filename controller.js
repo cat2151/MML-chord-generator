2ab2656d131fff5186dc4d7c0551ea1321d461a9
cat2151 <fct_2004@mail.goo.ne.jp>
2015-11-09 21:44:09 +0900

    URLからMMLを取得するようにしてみた

--- controller.js
+++ controller.js
@@ -1,14 +1,27 @@
 angular.module('generatorApp')
-.controller('generatorController', ['$scope', 'GeneratorService',
-function($scope, GeneratorService) {
+.controller('generatorController', ['$scope', '$location', 'GeneratorService',
+function($scope, $location, GeneratorService) {
 
   $scope.generatedMml = "なし";
+
+  function setMmlFromUrl() {
+    // [URLイメージ] ～/#?mml=C
+    var urlMml = $location.search().mml;
+    if (angular.isString(urlMml)) {
+      $scope.inputText = urlMml;
+      $scope.generate();
+    }
+  }
 
   $scope.generate = function() {
     $scope.generatedMml = GeneratorService.generate($scope.inputText);
     SIOPM.compile($scope.generatedMml);
   };
 
+  SIOPM.onLoad = function() {
+    setMmlFromUrl();
+  };
+        
   SIOPM.onCompileComplete = function() {
     SIOPM.play();
   };

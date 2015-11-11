27f0d24b0e1f05a65bb48ea823716f82744688db
cat2151 <fct_2004@mail.goo.ne.jp>
2015-11-11 22:37:02 +0900

    ChordNameパーサ実装中。CとBbをパースして0と10を得てみる

--- services.js
+++ services.js
@@ -7,9 +7,20 @@ function() {
     mml += ';';
     mml += inputText.replace(/C/g, 'c;e;g');
     return mml;
-  };
+  }
+  
+  function getRootNoteType(inputText) {
+    if (inputText.search(/Bb/) == 0) {
+      return 10;
+    }
+    if (inputText.search(/C/) == 0) {
+      return 0;
+    }
+    return -1;
+  }
 
   return {
-    generate: generate
+    generate: generate,
+    getRootNoteType: getRootNoteType
   };
 }]);

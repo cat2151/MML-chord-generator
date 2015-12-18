fb5f92dcdb3a3de5527a892f2b160c0915a85c0e
cat2151 <fct_2004@mail.goo.ne.jp>
2015-12-18 23:35:37 +0900

    数字でchordを指定する際、Key : C 固定だったのを、C以外を指定できるよう実装中。radioを実装した #23

--- controller.js
+++ controller.js
@@ -185,6 +185,21 @@ function($scope, $location, $timeout, GeneratorService) {
     $scope.generate();
   };
 
+  $scope.keyRadios = [
+    {id:0, name:"C"},
+    {id:1, name:"C#"},
+    {id:2, name:"D"},
+    {id:3, name:"D#"},
+    {id:4, name:"E"},
+    {id:5, name:"F"},
+    {id:6, name:"F#"},
+    {id:7, name:"G"},
+    {id:8, name:"G#"},
+    {id:9, name:"A"},
+    {id:10, name:"A#"},
+    {id:11, name:"B"}
+  ];
+
   SIOPM.onLoad = function() {
     if (angular.isString($scope.p.inputText)) {
       $timeout(function() {
